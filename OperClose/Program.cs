0bdfc8e6269c2cd2f6bc53605235ae3d833b46d6
DotnetTest1 <mjmdellpython@gmail.com>
2021-08-19 22:00:21 +0100

    init open/close concept

---
+++ OperClose/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OpenClose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //classOpenClose c = new classOpenClose(); Old virsion 
+            //c.Id = 1;
+            //c.Product = "T-shirt";
+            //c.Price = 5;
+
+            //Console.WriteLine(c.ToString() +
+            //                  Environment.NewLine+
+            //                  c.TotalPrice(2));
+
+            /* CONSTRUCTUR METHOD
+            productNoVAT pn = new productNoVAT();
+            pn.Id = 1;
+            pn.Price = 3;
+            pn.Product = "Dress";
+            Console.WriteLine(pn.ToString()+
+                              Environment.NewLine+
+                              pn.TotalPrice(1));
+
+
+            productWithVAT pw = new productWithVAT();
+            pw.Id = 1;
+            pw.Price = 3;
+            pw.Product = "Dress";
+            Console.WriteLine(pw.ToString() +
+                              Environment.NewLine +
+                              pw.TotalPrice(1));*/
+
+            /*INTERFACE METHOD*/
+            productNoVATi pn = new productNoVATi();
+            pn.Id = 1;
+            pn.Price = 3;
+            pn.Product = "Dress";
+            Console.WriteLine(pn.ToString() +
+                              Environment.NewLine +
+                              pn.Totalprice(1));
+
+
+            productWithVATi pw = new productWithVATi();
+            pw.Id = 1;
+            pw.Price = 3;
+            pw.Product = "Dress";
+            Console.WriteLine(pw.ToString() +
+                              Environment.NewLine +
+                              pw.Totalprice(1));
+
+        }
+    }
+}
---
+++ OperClose/classOpenClose.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenClose
+{
+    /*To make any change of pruducts we will need to change the main class 'classOpenClose'
+         , so to save it form any possible futre changes we should follow the oper / close
+         principle with SOLID
+        
+         There are two possible ways to make it
+        - Using abstract class
+        - Using interface
+        
+         */
+    public abstract class classOpenClose // Add abstract when using abstract way
+    {
+        
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+
+
+        public abstract decimal TotalPrice(int Vat); // and add abstract to maintain method and
+                                                     // delete the implement in method.
+        
+
+        public override string ToString()
+        {
+            return $"The order {Id} for product {Product}";
+        }
+    }
+
+
+    class productNoVAT : classOpenClose
+    {
+        public override decimal TotalPrice(int Vat)
+        {
+            {
+                
+                 return Price;
+                
+            }
+        }
+    }
+
+    class productWithVAT : classOpenClose
+    {
+        public override decimal TotalPrice(int Vat)
+        {
+            if (Price > 2)
+            {
+                return Price + Vat;
+            }
+            else
+                return Price;
+        }
+    }
+
+    /*when Using interface atteibute we should create interface contain the method that we will need
+     to implement the possible changes*/
+
+    interface Iproduct
+    {
+        decimal Totalprice(int VAT);
+    }
+
+    class productNoVATi : Iproduct
+    {
+
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Totalprice(int VAT)
+        {
+            return Price;
+        }
+        public override string ToString()
+        {
+            return $"The order {Id} for product {Product}";
+        }
+    }
+
+    class productWithVATi : Iproduct
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Totalprice(int VAT)
+        {
+            if (Price > 2)
+            {
+                return Price + VAT;
+            }
+            else
+                return Price;
+        }
+        public override string ToString()
+        {
+            return $"The order {Id} for product {Product}";
+        }
+    }
+
+} 
